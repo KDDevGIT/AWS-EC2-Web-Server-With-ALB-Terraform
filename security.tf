a1cf07a9991bc8ab04a04385538f8477e26e0baa
KDDevGIT <kymasterbox@gmail.com>
2025-09-08 12:03:43 -0700

    Added EC2 Security Group (Allow Port 80 from ALB SG Only) with defined ingress/egress to security.tf

--- security.tf
+++ security.tf
@@ -25,3 +25,30 @@ resource "aws_security_group" "alb" {
     Name = "${var.project_name}-alb-sg"
   }
 }
+
+# EC2 Security Group (Allow Port 80 only from ALB SG)
+resource "aws_security_group" "web" {
+  name = "${var.project_name}-web-sg"
+  description = "Security Group for Web Instances"
+  vpc_id = aws_vpc.this.id 
+
+  ingress {
+    description = "HTTP from ALB Security Group"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-web-sg"
+  }
+}
