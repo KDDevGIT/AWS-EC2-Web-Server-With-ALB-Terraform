3281a3abe8a2f5eb8bacbe937ea07bc814eed8b8
KDDevGIT <kymasterbox@gmail.com>
2025-09-08 12:08:34 -0700

    Added iam.tf and added SSM access to private instance to iam.tf

---
+++ iam.tf
@@ -0,0 +1,14 @@
+# SSM Session Manager Access to Private Instance
+resource "aws_iam_role" "ssm_role" {
+  name = "${var.project_name}-ssm-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+            Service = "ec2.amazonaws.com"
+        }
+    }]
+  })
+}
