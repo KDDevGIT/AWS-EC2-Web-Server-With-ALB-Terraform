4879a6b152b59652c0fbb60e6b51d1c26d383322
KDDevGIT <kymasterbox@gmail.com>
2025-09-08 13:23:25 -0700

    Added Auto-Scaling group to compute-alb.tf

--- compute-alb.tf
+++ compute-alb.tf
@@ -20,3 +20,23 @@ resource "aws_launch_template" "web" {
   }
 }
 
+resource "aws_autoscaling_group" "web" {
+  name = "${var.project_name}-asg"
+  desired_capacity = var.desired_capacity
+  max_size = var.max_size
+  min_size = var.min_size
+  vpc_zone_identifier = [for s in aws_subnet.private : s.id]
+  health_check_type = "ELB"
+  health_check_grace_period = 90
+  launch_template {
+    id = aws_launch_template.web.id 
+    version = "$Latest"
+  }
+  target_group_arns = [aws_lb_target_group.http.arn]
+
+  tag {
+    key = "Name"
+    value = "${var.project_name}-web"
+    propagate_at_launch = true
+  }
+}
