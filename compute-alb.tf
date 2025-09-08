6124ea70f9b1b8775a6ab42d93fcdb7b90b0ada5
KDDevGIT <kymasterbox@gmail.com>
2025-09-08 13:32:26 -0700

    Added load balancer with lb target group and lb listener to compute-alb.tf

--- compute-alb.tf
+++ compute-alb.tf
@@ -40,3 +40,43 @@ resource "aws_autoscaling_group" "web" {
     propagate_at_launch = true
   }
 }
+
+# Load Balancer
+resource "aws_lb" "this" {
+  name = "${var.project_name}-alb"
+  load_balancer_type = "application"
+  internal = false 
+  security_groups = [aws_security_group.alb.id]
+  subnets = [for s in aws_subnet_public : s.id]
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+# Load Balancer Target Group
+resource "aws_lb_target_group" "http" {
+  name = "${var.project_name}-tg"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.this.id 
+
+  health_check {
+    path = "/"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 15
+    timeout = 5
+    matcher = "200-399"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  } 
+}
